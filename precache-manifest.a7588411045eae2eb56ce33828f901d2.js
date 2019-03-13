746ece590207dcafe892d6269261e02f210f73c8
tam-nt24897 <sanghit2481997@gmail.com>
2019-03-13 20:31:56 +0700

    Updates

---
+++ precache-manifest.a7588411045eae2eb56ce33828f901d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cc9b059203060ac9057",
+    "url": "/react_crash_todo/static/js/runtime~main.045ec42f.js"
+  },
+  {
+    "revision": "2c3584dd998d37304358",
+    "url": "/react_crash_todo/static/js/main.fc18f0c5.chunk.js"
+  },
+  {
+    "revision": "efb128296f4f0ed3a42b",
+    "url": "/react_crash_todo/static/js/2.36c6890f.chunk.js"
+  },
+  {
+    "revision": "2c3584dd998d37304358",
+    "url": "/react_crash_todo/static/css/main.fa8e7b26.chunk.css"
+  },
+  {
+    "revision": "3405a7c396335501d60601beb5de0f2c",
+    "url": "/react_crash_todo/index.html"
+  }
+];
